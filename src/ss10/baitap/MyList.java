28b0516bc625d1380f2da381410370aa24232185
NguyenDanhh <danhnguyen15112003@gmai.com>
2023-02-28 16:53:42 +0700

    first commit

---
+++ src/ss10/baitap/MyList.java
@@ -0,0 +1,16 @@
+package ss10.baitap;
+
+public class MyList <E>{
+    private int size = 0 ;
+    static final int DEFAULT_CAPACITY = 10 ;
+    private Object elements[];
+    public MyList(){
+
+    }
+    public MyList( int capacity){
+        capacity = capacity;
+    }
+    public void add (int index , E element ){
+
+    }
+}
