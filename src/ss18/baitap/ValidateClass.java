62f1024e0437491d2ec0dda9490652189c32d06b
NguyenDanhh <danhnguyen15112003@gmai.com>
2023-03-10 11:45:34 +0700

    first commit

---
+++ src/ss18/baitap/ValidateClass.java
@@ -0,0 +1,25 @@
+package ss18.baitap;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateClass {
+    private static boolean checkClassName(String str) {
+        Pattern pattern = Pattern.compile("^[C || A || P][0-9]{4}[G || H || I || K]$");
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println("OK");
+        } else {
+            System.out.println("No");
+        }
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chuỗi cần kiểm tra ");
+        String str = scanner.nextLine();
+        System.out.println(checkClassName(str));
+    }
+}
---
+++ src/ss18/baitap/ValidatePhone.java
@@ -0,0 +1,21 @@
+package ss18.baitap;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatePhone {
+    public static boolean checkPhoneNumber(String str) {
+        Pattern pattern = Pattern.compile("^[(0-9)]{4}-[(0-9)]{12}$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số điện thoại cần kiểm tra");
+        String str = scanner.nextLine();
+        System.out.println(checkPhoneNumber(str));
+
+    }
+}
